073e128bc286c12c68f2b3b7cd2c9b3dd95056ad
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 11:24:01 +0100

    refacto - extraction de fonctions dans calculator

--- StringCalculatorFinal.Console/StringCalculator.cs
+++ StringCalculatorFinal.Console/StringCalculator.cs
@@ -5,13 +5,19 @@ public class StringCalculator
     public static int Add(string inputString)
     {
         var inputArray = inputString.Split(",");
+        var result = AddidtionFromArray(inputArray);
+        return result;
+    }
+
+    private static int AddidtionFromArray(string[] inputArray)
+    {
         var result = 0;
         List<List<int>> exceptionsPosition = [];
         var position = 0;
         foreach (var number in inputArray)
         {
             position += 1;
-            var numberWithoutSpaces = number.Replace(" ", "");
+            var numberWithoutSpaces = number.ToString().Replace(" ", "");
             var intNumber = int.Parse(numberWithoutSpaces);
 
             
@@ -24,11 +30,17 @@ public class StringCalculator
             result += intNumber;
         }
 
+        CheckNegativeNumber(exceptionsPosition);
+        return result;
+    }
+
+    private static void CheckNegativeNumber(List<List<int>> exceptionsPosition)
+    {
         if (exceptionsPosition.Count > 0)
         {
             throw new NombreNegatifException(exceptionsPosition);
         }
-        return result;
     }
+    
 }
 
