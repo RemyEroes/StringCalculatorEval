fbd4b75a081b3ba157f8461beb9c0bf28ae6714d
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 09:33:24 +0100

    [RED] nouveau test 0+1 ne passe pas

--- StringCalculatorFinal.Test/StringCaluclatorTest.cs
+++ StringCalculatorFinal.Test/StringCaluclatorTest.cs
@@ -15,5 +15,17 @@ public class StringCaluclatorTest
         var resultat = StringCalculator.Add(calculatorString);
         Assert.Equal(attendu,resultat);
     }
+    
+    [Fact]
+    public void Test0Plus1()
+    {
+        var a = 0;
+        var b = 1;
+        var calculatorString = $"{a},{b}";
+        var attendu = "1";
+
+        var resultat = StringCalculator.Add(calculatorString);
+        Assert.Equal(attendu,resultat);
+    }
 }
 
