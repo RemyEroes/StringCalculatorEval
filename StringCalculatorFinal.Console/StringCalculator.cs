e90a0f93da2e20264bf1277de8f07c68ede3fef3
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 10:42:21 +0100

    test des nombres négatifs passent

--- StringCalculatorFinal.Console/StringCalculator.cs
+++ StringCalculatorFinal.Console/StringCalculator.cs
@@ -6,17 +6,33 @@ public class StringCalculator
     {
         var inputArray = inputString.Split(",");
         var result = 0;
+        List<List<int>> exceptionsPosition = [];
+        var position = 0;
         foreach (var number in inputArray)
         {
+            position += 1;
             var numberWithoutSpaces = number.Replace(" ", "");
             var intNumber = int.Parse(numberWithoutSpaces);
 
-            if (intNumber < 0)
+            
+            if (intNumber < 0 )
             {
-                return "Des nombres négatifs sont présents: '-1' en position 1 ";
+                List<int> newExceptionPos = [intNumber, position];
+                exceptionsPosition.Add(newExceptionPos);
             }
             
             result += intNumber;
+        }
+
+        if (exceptionsPosition.Count > 0)
+        {
+            var exceptionString = "";
+            foreach (var exception in exceptionsPosition)
+            {
+                exceptionString+= $"'{exception[0]}' en position {exception[1]} ";
+            }
+
+            return "Des nombres négatifs sont présents: " + exceptionString;
         }
         return result.ToString();
     }
