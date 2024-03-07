1e7b95f242e2f56ccea461b0ed440002de61e299
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 09:40:09 +0100

    Refacto des test en theory A+B

--- StringCalculatorFinal.Test/StringCaluclatorTest.cs
+++ StringCalculatorFinal.Test/StringCaluclatorTest.cs
@@ -4,40 +4,17 @@ namespace StringCalculatorFinal.Test;
 
 public class StringCaluclatorTest
 {
-    [Fact]
-    public void Test0Plus0()
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(0, 1)]
+    [InlineData(1, 1)]
+    public void TestAPlusB(int a, int b)
     {
-        var a = 0;
-        var b = 0;
         var calculatorString = $"{a},{b}";
-        var attendu = "0";
-
+        var attendu = a + b;
+        
         var resultat = StringCalculator.Add(calculatorString);
-        Assert.Equal(attendu,resultat);
-    }
-    
-    [Fact]
-    public void Test0Plus1()
-    {
-        var a = 0;
-        var b = 1;
-        var calculatorString = $"{a},{b}";
-        var attendu = "1";
-
-        var resultat = StringCalculator.Add(calculatorString);
-        Assert.Equal(attendu,resultat);
-    }
-    
-    [Fact]
-    public void Test1Plus1()
-    {
-        var a = 1;
-        var b = 1;
-        var calculatorString = $"{a},{b}";
-        var attendu = "2";
-
-        var resultat = StringCalculator.Add(calculatorString);
-        Assert.Equal(attendu,resultat);
+        Assert.Equal(attendu.ToString(),resultat);
     }
 }
 
