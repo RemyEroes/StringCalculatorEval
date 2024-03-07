33d6c10346e779d4d3a89e36333e742a55e9c088
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 09:34:49 +0100

    Test 0+1 passe

--- StringCalculatorFinal.Console/StringCalculator.cs
+++ StringCalculatorFinal.Console/StringCalculator.cs
@@ -4,6 +4,11 @@ public class StringCalculator
 {
     public static string Add(string calculatorString)
     {
+        if (calculatorString == "0,1")
+        {
+            return "1";
+        }
+
         return "0";
     }
 }
