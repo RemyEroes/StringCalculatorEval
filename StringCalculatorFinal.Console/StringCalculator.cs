6cc584d3c85ecf9633678a80a707345300df99d9
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2024-03-07 09:30:17 +0100

    [RED] test de 0+0 ne passe pas

---
+++ StringCalculatorFinal.Console/StringCalculator.cs
@@ -0,0 +1,9 @@
+namespace StringCalculatorFinal.Console;
+
+public class StringCalculator
+{
+    public static IEnumerable<char> Add(string calculatorString)
+    {
+        throw new NotImplementedException();
+    }
+}
--- StringCalculatorFinal.Test/StringCaluclatorTest.cs
+++ StringCalculatorFinal.Test/StringCaluclatorTest.cs
@@ -1,9 +1,19 @@
+using StringCalculatorFinal.Console;
+
 namespace StringCalculatorFinal.Test;
 
 public class StringCaluclatorTest
 {
     [Fact]
-    public void Test1()
+    public void Test0Plus0()
     {
+        var a = 0;
+        var b = 0;
+        var calculatorString = $"{a},{b}";
+        var attendu = "0";
+
+        var resultat = StringCalculator.Add(calculatorString);
+        Assert.Equal(attendu,resultat);
     }
 }
+
